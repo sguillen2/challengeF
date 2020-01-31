cdda2af329db1bf569a1a63d20f6d5bc80d199fe
sguillen2 <45013630+sguillen2@users.noreply.github.com>
2020-01-31 15:13:11 -0800

    Add files via upload

---
+++ challenge F.rb
@@ -0,0 +1,14 @@
+banger ="C:/Users/sonny_guillen/Downloads/challenge_f/this_is_a_banger.wav"
+
+sample banger , rate: 0.9
+sleep 3
+sample banger , rate: 0.8
+sleep 3
+sample banger , rate: 0.7
+sleep 3
+sample banger , rate: 0.6
+sleep 3
+sample banger , rate: 1
+sleep 3
+sample banger , rate: 2
+
